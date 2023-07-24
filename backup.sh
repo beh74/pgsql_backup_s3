4da6f135553ccb5967e7300d6549ca67627583fd
Bertrand HARTWIG <hartwig.bertrand@gmail.com>
2023-07-24 08:19:09 +0200

    Update backup.sh - bug fixes

--- backup.sh
+++ backup.sh
@@ -137,28 +137,35 @@ fi
 echo "Connecting PostgreSql server to get the server version"
 sqlver=`psql -t -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DATABASE -c "SHOW server_version;"`
 
+# get database size in bytes
+dbsize=`psql -t -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DATABASE -c "select pg_size_pretty(pg_database_size('${POSTGRES_DATABASE}'));"`
+
 echo "Creating dump of all databases from ${POSTGRES_HOST} version ${sqlver}..."
 start=`date +%s`
-pg_dumpall -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER | gzip > /home/bckuser/dump.sql.gz
+#pg_dumpall -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER | gzip > /home/bckuser/dump.sql.gz
+pg_dump -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DATABASE -v -C -Z 6 -F d -j 4 -b -f  /home/bckuser/$BACKUP_FILENAME
+# create a tar file for $POSTGRES_DATABASE folder
+tar -cvf "/home/bckuser/${BACKUP_FILENAME}.tar" "/home/bckuser/${BACKUP_FILENAME}"
 end=`date +%s`
 dump_runtime=$((end-start))
-filesize=$(stat -c%s "/home/bckuser/dump.sql.gz")
+filesize=$(stat -c%s "/home/bckuser/${BACKUP_FILENAME}.tar")
 echo "pg_dumpall took ${dump_runtime} seconds. Filesize is ${filesize} bytes"
 
-echo "Uploading dump to s3://${S3_BUCKET}/${S3_PREFIX}${BACKUP_FILENAME}.sql.gz"
+echo "Uploading dump to s3://${S3_BUCKET}/${S3_PREFIX}${BACKUP_FILENAME}.tar"
 start=`date +%s`
-cat /home/bckuser/dump.sql.gz | aws $AWS_ARGS s3 cp - "s3://${S3_BUCKET}/${S3_PREFIX}${BACKUP_FILENAME}.sql.gz" || exit 2
+cat /home/bckuser/${BACKUP_FILENAME}.tar | aws $AWS_ARGS s3 cp - "s3://${S3_BUCKET}/${S3_PREFIX}${BACKUP_FILENAME}.tar" || exit 2
 end=`date +%s`
 s3_runtime=$((end-start))
 echo "s3 upload took ${s3_runtime} seconds"
 
 aws $AWS_ARGS s3api put-object-tagging \
     --bucket ${S3_BUCKET} \
-    --key "${S3_PREFIX}${BACKUP_FILENAME}.sql.gz" \
+    --key "${S3_PREFIX}${BACKUP_FILENAME}.tar" \
     --tagging '{"TagSet": [{ "Key": "dump-time", "Value": "'"$dump_runtime"'" }, { "Key": "dump-type", "Value": "postgresql" }, { "Key": "database-version", "Value": "'"$sqlver"'" }]}'
 
 echo "SQL backup uploaded successfully"
-rm -rf /home/bckuser/dump.sql.gz
+rm -rf /home/bckuser/${BACKUP_FILENAME}.tar
+rm -rf /home/bckuser/${BACKUP_FILENAME}
 
 if [ "${ELASTIC_URL}" = "**None**" ]; then
     echo "Elasticsearch URL not provided"
@@ -174,10 +181,11 @@ else
     "database.host": "'"${POSTGRES_HOST}"'",
     "database.port": "'"${POSTGRES_PORT}"'",
     "database.user": "'"${POSTGRES_USER}"'",
+    "database.dbsize": "'$dbsize'",
     "dump.duration": '"$dump_runtime"',
     "dump.filesize": '"$filesize"',
     "dump.status": "successfull",
-    "dump.filename": "'"${BACKUP_FILENAME}"'",
+    "dump.filename": "'"${BACKUP_FILENAME}.tar"'",
     "s3.bucket": "'"${S3_BUCKET}"'",
     "s3.prefix": "'"${S3_PREFIX}"'",
     "s3.endpoint": "'"${S3_ENDPOINT}"'",
